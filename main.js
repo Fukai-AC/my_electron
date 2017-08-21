2cb8ca7f27925ad2657cd888042e188488cfdfbe
wangfukai <18514635524@163.com>
2017-08-21 11:02:43 +0800

    add update target

--- main.js
+++ main.js
@@ -5,66 +5,84 @@ const {
   MenuItem,
   ipcMain,
   ipcRenderer,
+  dialog
 } = require('electron');
+const { version } = require('./package.json');
+const electron = require('electron');
 const path = require('path');
 const url = require('url');
+const axios = require('axios');
 
 // 保持一个对于 window 对象的全局引用，如果你不这样做，
 // 当 JavaScript 对象被垃圾回收， window 会被自动地关闭
 let win;
+const check_update = () => {
+  axios.get().end((err, res) => {
+    if(res.statusCode == 204) {
+
+    }
+  });
+  const index = dialog.showMessageBox({
+    type: 'info',
+    message: '发现新版本请立即更新',
+    buttons: ['升级', '取消']
+  });
+  console.log(index);
+}
 
 function createWindow () {
+  console.log(version);
   global.ipcRender = ipcRenderer;
   // 创建浏览器窗口。
   win = new BrowserWindow({
-    width: 1300,
-    height: 800,
+    width: 930,
+    height: 580,
     resizable: false,
     movable: true,
-    enableLargerThanScreen: false,
     maximizable: false,
     frame: false,
+    show: false,
+    backgroundColor: '#CDFEFF',
   });
-  // 加载应用的 index.html。
-  win.loadURL('http://localhost:5000/home', {
+  win.once('ready-to-show', () => {
+    win.show()
+  })
+  win.webContents.on('new-window', (ev, url) => {
+    ev.preventDefault();
+    const displays = electron.screen.getAllDisplays();
+    let externalDisplay = displays.find((display) => {
+      return display.bounds.x !== 0 || display.bounds.y !== 0
+    })
+    var window = new BrowserWindow({
+      width: externalDisplay.workAreaSize.width,
+      height: externalDisplay.workAreaSize.height,
+      maximize: false,
+    })
+    window.setFullScreen(true)
+    window.maximize();
+    window.loadURL(url);
+  });
+  win.loadURL('http://192.168.30.235:5050/home', {
     userAgent: 'codemao-application'
   });
-  
-  // 当 window 被关闭，这个事件会被触发。
   win.on('closed', () => {
-    // 取消引用 window 对象，如果你的应用支持多窗口的话，
-    // 通常会把多个 window 对象存放在一个数组里面，
-    // 与此同时，你应该删除相应的元素。
     win = null
   });
+  // check_update();
 }
 
-// Electron 会在初始化后并准备
-// 创建浏览器窗口时，调用这个函数。
-// 部分 API 在 ready 事件触发后才能使用。
 app.on('ready', createWindow);
-
-// 当全部窗口关闭时退出。
 app.on('window-all-closed', () => {
-  // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
-  // 否则绝大部分应用及其菜单栏会保持激活。
   if (process.platform !== 'darwin') {
     app.quit()
   }
 });
 
 app.on('activate', () => {
-  // 在这文件，你可以续写应用剩下主进程代码。
-  // 也可以拆分成几个文件，然后用 require 导入。
   if (win === null) {
     createWindow()
   }
 });
-
-// 在这文件，你可以续写应用剩下主进程代码。
-// 也可以拆分成几个文件，然后用 require 导入。
-
-
 
 const menu = new Menu();
 menu.append(new MenuItem({
@@ -82,7 +100,13 @@ ipcMain.on('show-context-menu', (event) => {
   const win = BrowserWindow.fromWebContents(event.sender);
   menu.popup(win);
 });
-ipcMain.on('hide_window', (event) => {
+ipcMain.on('minimize_window', (event) => {
   const win = BrowserWindow.fromWebContents(event.sender);
   win.minimize();
+});
+ipcMain.on('close_window', (event) => {
+  app.quit();
+});
+ipcMain.on('clear_cache', (event) => {
+  win.webContents.clearHistory();
 });
