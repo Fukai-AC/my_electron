19c0f9718bfba85b88b91467a8c7deeadedbe2ba
wangfukai <wangfukai@codemao.cn>
2017-09-06 16:27:32 +0800

    add dev url

--- main.js
+++ main.js
@@ -82,7 +82,7 @@ function createWindow() {
   });
   win.openDevTools();
   win.webContents.on('new-window', new_window_listener);
-  win.loadURL('http://localhost:5050/home', {
+  win.loadURL('https://dev-client.codemao.cn/home', {
     userAgent: 'codemao-application'
   });
   win.on('closed', () => {
